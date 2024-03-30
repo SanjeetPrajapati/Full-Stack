b57e780a9cf6b27c339dc09bebbe1ce332dc749e
Sanjeet Prajapati <prajapatisanjeet50@gmail.com>
2024-03-30 22:07:15 +0530

    day 2

---
+++ JS/2.js
@@ -0,0 +1,17 @@
+for( let i=1;i<=5;i++) {
+    console.log(Math.random() )
+}
+// For range
+console.log("for range")
+console.log(Math.floor(Math.random() * 11))
+
+const myObj = {
+    name : "sanjeet",
+    roll : "41",
+    clss : "TE",
+}
+console.log(myObj);
+console.log(myObj.name);
+myObj.surname="prajapati"
+console.log(myObj);
+
